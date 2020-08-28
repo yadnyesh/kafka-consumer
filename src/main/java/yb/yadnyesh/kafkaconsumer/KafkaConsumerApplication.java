193be3115c0fa4faa4c0f0c6fc42fe2eddcdc616
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-29 01:14:56 +0530

    Project Setup Commit

---
+++ src/main/java/yb/yadnyesh/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+
+}
