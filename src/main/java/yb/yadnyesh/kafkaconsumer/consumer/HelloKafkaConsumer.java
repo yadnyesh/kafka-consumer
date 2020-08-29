b0de4ce7b08e71580f17aab03409ace49a36eb1d
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-29 12:04:32 +0530

    Kafka Consumer in progress

---
+++ src/main/java/yb/yadnyesh/kafkaconsumer/consumer/HelloKafkaConsumer.java
@@ -0,0 +1,14 @@
+package yb.yadnyesh.kafkaconsumer.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+	
+	@KafkaListener(topics = "t_hello")
+	public void consumeFromKafka(String message) {
+		System.out.println(message);
+	}
+
+}
