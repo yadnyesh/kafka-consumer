f77a3710d1a7c7b89a682d7b81cad550c0ed7f15
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-29 13:31:32 +0530

    Consumer NOT comsuming produced messages

---
+++ src/main/java/yb/yadnyesh/kafkaconsumer/consumer/FixedRateConsumer.java
@@ -0,0 +1,18 @@
+package yb.yadnyesh.kafkaconsumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateConsumer {
+	
+	private static final Logger log = LoggerFactory.getLogger(FixedRateConsumer.class);
+	
+	@KafkaListener(topics = "t_fixedRate")
+	public void consumeMessageFromKafka(String message) {
+		log.info("Consuming:  {}", message);
+	}
+
+}
